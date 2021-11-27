3cff480e0c02c0270ebfc66cb545efc298700671
Kazymir Rabier <kazymir.rabier@gmail.com>
2021-11-27 18:47:40 +0800

    adding hero in home page

---
+++ src/pages/home/Home.js
@@ -0,0 +1,25 @@
+import landing from "../../assets/landing.jpg";
+
+function Home() {
+  return (
+    <div className=''>
+      <img src={landing} alt='' className='h-screen w-full object-cover' />
+      <div className='bg-gray-800 absolute top-0 h-screen w-full bg-opacity-40 z-10'>
+        <div className='container text-gray-100 z-20 h-full flex flex-col items-start justify-center'>
+          <div className='max-w-lg'>
+            <h1 className='mb-5'>Discover another side of Singapore.</h1>
+            <h4 className='mb-5'>
+              Artefacts from the past documenting life in and around post-war
+              Singapore from the perspective of Ivan Polunin.
+            </h4>
+            <button className='bg-gray-900 tracking-wider font-Playfair py-3 px-5 rounded-sm hover:bg-gray-800'>
+              About
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+      padding: '1.5rem',
+    },
     extend: {
       fontFamily: {
         Playfair: ["Playfair Display", "serif"],
